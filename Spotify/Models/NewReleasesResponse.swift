5c476b05a1ee97e6bae38b7c0ab63d29a50244ef
JustHiro55 <hagi.hiro9829@gmail.com>
2021-11-11 23:28:48 +0900

    forsave

---
+++ Spotify/Models/NewReleasesResponse.swift
@@ -0,0 +1,28 @@
+//
+//  NewReleaseResponse.swift
+//  Spotifyfy
+//
+//  Created by Hagiwara Hiromichi on 2021/11/11.
+//
+
+import Foundation
+
+struct NewReleasesResponse: Codable {
+    let albums: AlbumsResponse
+}
+
+struct AlbumsResponse: Codable {
+    let items: [Album]
+}
+
+struct Album: Codable {
+    let album_type: String
+    let available_markets: [String]
+    let id: String
+    let images: [APIImage]
+    let name: String
+    let release_data: String
+    let total_tracks: Int
+    let artists: [Artist]
+}
+
